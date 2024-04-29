51d17c2798278c935d1723b81a372352d5717d03
damanpreet <damanpreet.singh@celebaltech.com>
2024-04-29 17:10:28 +0530

    models added

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,52 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  email: string;
+  userName: string;
+  firstName?: string;
+  lastName?: string;
+  photo: string;
+  planId?: number;
+  creditBalance?: number;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  planId: {
+    type: Number,
+    required: false,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    required: false,
+    default: 10,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
